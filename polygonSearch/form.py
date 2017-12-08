6a764dd4d79666ecdc56044e83d5c2b4a4e22fc6
unknown <thomas.angelini@hotmail.fr>
2017-12-08 16:53:29 +0100

    change readme+updateUser

--- polygonSearch/form.py
+++ polygonSearch/form.py
@@ -11,3 +11,7 @@ class RegisterForm(forms.Form):
     password = forms.CharField(label="Saisir votre mot de passe (8 caractères minimum)", max_length=30, min_length=8)
     passwordVerif = forms.CharField(label="Contrôle du mot de passe (saisir le même mot de passe", max_length=30, min_length=8)
 
+class EditForm(forms.Form):
+    username = forms.CharField(label="Votre nouveau nom d'utilisateur", max_length=30)
+    password = forms.CharField(label="Votre nouveau mot de passe", widget=forms.PasswordInput)
+    passwordVerif = forms.CharField(label="Contrôle du mot de passe (saisir le même mot de passe", max_length=30, min_length=8)
