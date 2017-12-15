32c54471e650098af7e11c4b016e8278ea58e033
franckobe <franck.garros@ynov.com>
2017-12-15 15:04:32 +0100

    resolving urlresolver

--- polygonSearch/templatetags/base_extras.py
+++ polygonSearch/templatetags/base_extras.py
@@ -6,7 +6,7 @@ register = template.Library()
 @register.simple_tag
 def active_page(request, view_name):
 
-    from django.core.urlresolvers import resolve, Resolver404
+    from django.urls import resolve, Resolver404
     path = resolve(request.path_info)
     if not request:
         return ""
