64fa32f765e237ff8410fc6da6800a4ecc361fc1
unknown <thomas.angelini@hotmail.fr>
2018-01-12 10:13:56 +0100

    début ajout DRF

--- polygonSearch/urls.py
+++ polygonSearch/urls.py
@@ -1,10 +1,13 @@
 from django.conf.urls import url
 from polygonSearch import views
+from rest_framework.authtoken import views as apiViews
 
 urlpatterns = [
     url(r'^login$', views.login_view, name='login'),
     url(r'^login/', views.login_view, name='login'),
     url(r'^logout$', views.logout_view, name='logout'),
     url(r'^search', views.search_view, name='search'),
-    url(r'^register$', views.register_view, name='register')
+    # url(r'^register$', apiViews.obtain_auth_token.register_view, name='register')
+    url(r'^register$', views.register_view, name='register'),
+    url(r'^authenticate$', apiViews.obtain_auth_token)
 ]
