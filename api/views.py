54817479f19d07fe45964e5f600ab6a121f6529d
unknown <laurent.martinez@ynov.com>
2018-02-02 15:58:13 +0100

    api et debut du crud

--- api/views.py
+++ api/views.py
@@ -1,6 +1,11 @@
 from django.contrib.auth.models import User, Group
-from rest_framework import viewsets
+from rest_framework import viewsets, status, permissions
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from api.permissions import IsOwnerOrReadOnly
 from api import serializers
+from api.serializers import KeywordSerializer
+from polygonSearch.models import Website_page, Website_word
 
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -19,3 +24,31 @@ class GroupViewSet(viewsets.ModelViewSet):
     serializer_class = serializers.GroupSerializer
 
 
+@api_view(['GET', 'POST'])
+def word_list(request, sort=None, desc=None):
+    """ get all the key words by sorting
+    :param object request:
+    :param str sort: field to sort
+    :param boolean desc: if descending
+    return the word list sorted or not"""
+    if request.method == 'GET':
+        wordList = Website_word.objects.all()
+        if sort:
+            if desc:
+                wordList = wordList.sort('-' + sort)
+            else:
+                wordList = wordList.sort(sort)
+        serializer = KeywordSerializer(wordList, many=True)
+        return Response(serializer.data)
+
+
+@api_view(['GET', 'DELETE'])
+def word(request, word=None):
+    if request.method == 'GET':
+        result = Website_word.objects.get(word=word)
+        serializer = KeywordSerializer(result, many=False)
+        return Response(serializer.data)
+    elif request.method == 'DELETE':
+        result = Website_word.objects.get(word=word)
+        result.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
