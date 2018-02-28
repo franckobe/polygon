ce2487ed5a6e7b3a05ed571c9d0a15e8deb1126e
Bruno Guignard <guignardbruno@hotmail.fr>
2018-02-28 09:55:33 +0100

    added links follow check and titles weight

--- crawlygon/models.py
+++ crawlygon/models.py
@@ -1,3 +1,4 @@
+# coding: utf8
 import re
 import urllib3
 from django.db import models
@@ -25,17 +26,23 @@ class PagesGetter:
     bwl = []
     # the name of the domaine IE www.toto.com
     domainName = ''
-    #author of the page
+    # author of the page
     author = ''
-
+    # meta robot tag
+    metaRobot = ''
 
     def __init__(self, url):
         self.url = url
+        self.tags = []
         # fills bad words array
         self.initializeBadWordsList()
         # get the page + fills tags, title and links
         self.getPage()
         self.saveThis()
+        # reset all arrays
+        self.tags = []
+        self.bwl = []
+        self.links = []
 
     def saveThis(self):
         # saving author
@@ -56,11 +63,13 @@ class PagesGetter:
         # saving pages
         page = Website_page(url=self.url, title=self.title, id_website_domain_id=dom.id_website_domain)
         try:
-            page.save()
+            ps = page.save()
+            page = Website_page.objects.last()
         except:
             #todo : en fait vérifier la date !!
-            exit()
-        page = Website_page.objects.last()
+            print('!!! erreur !!!')
+            return
+
         # saving words + weights
         for tag in self.tags:
             ws = Website_word(word=tag.get('text'), weight=tag.get('weight'), url=self.url, id_website_page_id=page.id_website_page)
@@ -73,18 +82,129 @@ class PagesGetter:
             except:
                 pass
 
+    # load the HTML page and lauches all functions
     def getPage(self):
         http = urllib3.PoolManager()
         r = http.request('GET', self.url)
         if r.status == 200:
-            # todo : check if the meta robots is noodp
             self.getDomain()
             self.pr = r.data
             self.pr = html.fromstring(self.pr)
-            self.getMeta()
-            self.getLinks()
-            self.getTags()
+            if self.getMeta() == 1:
+                self.getTitle()
+                # if the owner doesn't want links to be indexed, we don't index them
+                if self.metaRobot.find('nofollow') == -1:
+                    self.getLinks()
+                self.getTags()
 
+    # get all tags containing text and having a weight
+    def getTags(self):
+        self.getTag('h1')
+        self.getTag('h2')
+        self.getTag('h3')
+        self.getTag('h4')
+        self.getTag('p')
+        self.getTag('span')
+        self.getTag('div')
+        self.getTag('strong')
+        if len(self.title) > 0:
+            self.getWords(self.title, 'title')
+
+    # For each tag, get words and treat them
+    def getTag(self, tagName):
+        for tag in self.pr.xpath("//" + tagName):
+            txt = str(tag.text)
+            self.getWords(txt, tagName)
+
+    # get all given words and tagname and apply weights etc
+    def getWords(self, txt, tagName):
+        # only get words
+        wl = re.sub("[^\w]", " ", txt.lower()).split()
+        # iterate each word
+        for word in wl:
+            # check if word is in stop words
+            if word not in self.bwl:
+                # check if already exist, if yes, we add weight
+                j = self.getWordIndex(word)
+                if j == -1:
+                    self.tags.append({'text': str(word), 'weight': str(self.getWeight(tagName))})
+                else:
+                    self.tags[j]['weight'] = str(int(self.tags[j]['weight']) + int(self.getWeight(tagName)))
+
+    # get the index of a word
+    def getWordIndex(self, word):
+        for i in range(len(self.tags)):
+            if self.tags[i]['text'] == word:
+                return i
+        return -1
+
+    # getting links
+    def getLinks(self):
+        for link in self.pr.xpath("//a"):
+            if "href" in link.attrib:
+                if len(link.attrib['href']) > 0:
+                    href = str(link.attrib['href'])
+                    if not href[0] == '#':
+                        # reformating links
+                        # if link begins by /x we add it to the domain name
+                        if href[0] == '/':
+                            self.links.append(self.domainName + str(link.attrib['href']))
+                            # if link begins by www, http:// we keep it
+                        else:
+                            self.links.append(link.attrib['href'])
+
+    # getting informations about meta tag
+    def getMeta(self):
+        for meta in self.pr.xpath("//meta"):
+            author = ''
+            if "name" in meta.attrib:
+                name = str(meta.attrib['name'])
+                if name.lower() == "robots":
+                    content = str(meta.attrib['content'])
+                    self.metaRobot = content.lower()
+                    if not content.lower().find('noindex') == -1 or not content.lower().find('none') == -1:
+                        return 0
+                if name.lower() == "author":
+                    author = str(meta.attrib['content'])
+                    author = author.lower()
+            self.author = author
+        return 1
+
+    # getting the title of the page
+    def getTitle(self):
+        ttl = self.url
+        for title in self.pr.xpath("//title"):
+            ttl = title.text.strip()
+        self.title = ttl
+
+    # getting the adress of the domain
+    def getDomain(self):
+        p = re.compile('^(https?:\/\/)?([\da-z\.-]+)')
+        c = p.findall(self.url)
+        self.domainName = str(c[0][0] + c[0][1])
+
+    # adding a weight for all words and all tags
+    def getWeight(self, tagName):
+        if tagName == 'h1':
+            return 40
+        elif tagName == 'h2':
+            return 30
+        elif tagName == 'title':
+            return 30
+        elif tagName == 'h3':
+            return 20
+        elif tagName == 'h4':
+            return 10
+        elif tagName == 'strong':
+            return 5
+        elif tagName == 'p':
+            return 1
+        elif tagName == 'span':
+            return 1
+        elif tagName == 'div':
+            return 1
+
+    # makes a list of unwanted useless words
     def initializeBadWordsList(self):
         self.bwl = ['je',
                     'tu',
@@ -146,6 +266,7 @@ class PagesGetter:
                     'd\'',
                     'se',
                     's\'',
+                    'aux',
                     'alors',
                     'au',
                     'aucuns',
@@ -192,6 +313,7 @@ class PagesGetter:
                     'quelles',
                     'quels',
                     'qui',
+                    'qu',
                     'seulement',
                     'si',
                     'sont',
@@ -220,95 +342,20 @@ class PagesGetter:
                     'm',
                     'c',
                     's',
+                    'none',
                     'ces']
-
-    def getTags(self):
-        self.getTag('h1')
-        self.getTag('h2')
-        self.getTag('h3')
-        self.getTag('h4')
-        self.getTag('p')
-
-    def getTag(self, tagName):
-        for tag in self.pr.xpath("//" + tagName):
-            txt = str(tag.text)
-            # only get words
-            wl = re.sub("[^\w]", " ", txt.lower()).split()
-            # iterate each word
-            for word in wl:
-                # check if word is in stop words
-                if word not in self.bwl:
-                    # check if already exist, if yes, we add weight
-                    j = self.getWordIndex(word)
-                    if j == -1:
-                        self.tags.append({'text': str(word), 'weight': str(self.getWeight(tagName))})
-                    else:
-                        self.tags[j]['weight'] = str(int(self.tags[j]['weight']) + int(self.getWeight(tagName)))
-
-    def getWordIndex(self, word):
-        for i in range(len(self.tags)):
-            if self.tags[i]['text'] == word:
-                return i
-        return -1
-
-    def getLinks(self):
-        for link in self.pr.xpath("//a"):
-            if "href" in link.attrib:
-                if len(link.attrib['href']) > 0:
-                    href = str(link.attrib['href'])
-                    if not href[0] == '#':
-                        # reformating links
-                        # if link begins by /x we add it to the domain name
-                        if href[0] == '/':
-                            self.links.append(self.domainName + str(link.attrib['href']))
-                            # if link begins by www, http:// we keep it
-                        else:
-                            self.links.append(link.attrib['href'])
-
-    def getMeta(self):
-        for meta in self.pr.xpath("//meta"):
-            author = ''
-            name = ''
-            if "name" in meta.attrib:
-                name = str(meta.attrib['name'])
-                if name.lower() == "robots":
-                    content = str(meta.attrib['content'])
-                    #todo : gérer le cas du nofollow
-                    if not content.lower().find('noindex') == -1:
-                        exit()
-                if name.lower() == "author":
-                    author = str(meta.attrib['author'])
-                    author = author.lower()
-            self.author = author
-
-    def getDomain(self):
-        p = re.compile('^(https?:\/\/)?([\da-z\.-]+)')
-        c = p.findall(self.url)
-        self.domainName = str(c[0][0] + c[0][1])
-
-    def getWeight(self, tagName):
-        if tagName == 'h1':
-            return 40
-        elif tagName == 'h2':
-            return 30
-        elif tagName == 'h3':
-            return 20
-        elif tagName == 'h4':
-            return 10
-        elif tagName == 'p':
-            return 1
 
 
 class CrawlerInstructor:
-    iterations = 200
+    iterations = 0
 
-    def __init__(self, nbIterations):
+    def __init__(self, nbIterations=200):
         self.iterations = nbIterations
         self.crawl()
 
     def crawl(self):
         # select the number of self.iterations of links to visit
-        ltv = Website_link_to_visit.objects.all().filter(visited_at__isnull=True)[:self.iterations]
+        ltv = Website_link_to_visit.objects.all().filter(visited_at__isnull=True)[:int(self.iterations)]
 
         for link in ltv:
             # Update the time
